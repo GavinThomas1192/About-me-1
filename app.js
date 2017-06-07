8c91e6fbcab2e7608dbe5d7c5195bc215eb98d8f
Ashley Durbin <ashleydurbin@Ashleys-MacBook-Air.local>
2017-06-07 08:30:39 -0700

    Added JAvascript html and css style

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict';
+function addListeners(){}
+var que1 = prompt("What is your name ")
+alert("Welcome to my web  page " + que1)
+
+var que2 = prompt(que1 + " Are you reday to get on this journey").toUpperCase();
+if (que2==="YES") { alert("Let's start this journey ")
+} else { alert("come back when you are ready")
+};
+
+var que3 = prompt(" Lets get personal Do u like dogs ?").toUpperCase();
+if (que3==="YES") { alert("You are awsome")
+} else { alert("I guess you are a cat person then ")
+};
+Console.log("user name is " + que1 + "and is likes dogs")
